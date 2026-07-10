3dfb7752232d979cc8caa6aaeb13a91cfcf5e6f9
Danilo Carlos <danilosilva9039-byte>
2026-07-10 20:53:12 -0300

    Nyquist avaliation added

---
+++ avaliaNyquist.m
@@ -0,0 +1,27 @@
+function avaliaNyquist()
+
+planta = obterPlantaReatorNuclear;
+[Gtheta, Gv] = obterTFReatividade(planta);
+G0 = obterFTReatorPotenciaZero(planta);
+
+beta = calculaBeta(planta);
+N0 = planta.N0;
+alphatheta = planta.alphatheta;
+
+F = feedback(G0/beta,Gtheta*N0*alphatheta,+1);
+
+L0 = -(N0*(G0/beta)*Gv)/(1 - N0*(G0/beta)*alphatheta*Gtheta);
+
+alpha = 0.002:0.0005:0.005;
+
+figure
+hold on
+
+for k=1:length(alpha)
+    L = alpha(k)*L0;
+    nyquist(L)
+end
+
+grid on
+
+end
