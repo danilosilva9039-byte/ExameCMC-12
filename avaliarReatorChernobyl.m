639e6fad701e865a160046f1c0bc0c0d45486f41
Danilo Carlos <danilosilva9039-byte>
2026-07-09 22:10:28 -0300

    Esqueci de upar o simulink, RS

---
+++ avaliarReatorChernobyl.m
@@ -0,0 +1,54 @@
+function avaliarReatorChernobyl()
+% faz a simulacao para atestar que o sistema
+% de chernobyl foi falho
+
+t_final = 10; 
+passo = 0.01;
+s = tf('s');
+t = 0:passo:t_final;
+
+planta = obterPlantaReatorNuclear;
+beta = calculaBeta(planta);
+
+atuador = 1 /s/s; % a ser alterado
+[y_tf, t_tf] = step(atuador, t); % a ser alterado
+dpex = timeseries(y_tf, t_tf);
+
+[Gtheta, Gv] = obterTFReatividade(planta);
+G0 = obterFTReatorPotenciaZero(planta);
+
+[numG0, denG0] = tfdata(G0, 'v');
+[numGv, denGv] = tfdata(Gv, 'v');
+[numGtheta, denGtheta] = tfdata(Gtheta, 'v');
+
+%assignin('base', 'planta', planta);
+
+reator_chernobyl = 'ReatorChernobyl';
+load_system(reator_chernobyl);
+
+in = Simulink.SimulationInput(reator_chernobyl);
+in = in.setModelParameter('StopTime', num2str(t_final));
+in = in.setVariable('dpex', dpex);
+in = in.setVariable('numG0', numG0);
+in = in.setVariable('denG0', denG0);
+in = in.setVariable('numGv', numGv);
+in = in.setVariable('denGv', denGv);
+in = in.setVariable('numGtheta', numGtheta);
+in = in.setVariable('denGtheta', denGtheta);
+in = in.setVariable('planta', planta);
+in = in.setVariable('beta', beta);
+
+simulacao = sim(in);
+
+saida = simulacao.NeutronVar;
+    
+figure;
+plot(saida.Time, saida.Data, 'LineWidth', 2);
+grid on;
+xlabel('Tempo (s)', 'FontSize', 12);
+ylabel('Variação Relativa (\delta n / n)', 'FontSize', 12);
+title('Resposta do Reator à Inserção de Reatividade', 'FontSize', 14);
+
+
+
+end
