23c678d722722dabc949f12a6219304e00311566
Danilo Carlos <danilosilva9039-byte>
2026-07-11 22:13:50 -0300

    Requirements Implemented

--- obterRequisitos.m
+++ obterRequisitos.m
@@ -1,4 +1,7 @@
 function requisitos = obterRequisitos(planta)
 %obtem os requisitos 
-	requisitos = [1,2 , 3]; % a ser alterado
+	requisitos.controlador.wb = 0.4; % rad/s
+    requisitos.controlador.PM = 60 % em graus
+    requisitos.controlador.GM = 11 % em dB
+
 end
