a1d0158340b9a6fb61a927dd9fd4c88a2c6261f7
Danilo Carlos <danilosilva9039-byte>
2026-07-11 23:51:40 -0300

    Minor Changes

--- obterRequisitos.m
+++ obterRequisitos.m
@@ -1,7 +1,7 @@
-function requisitos = obterRequisitos(planta)
+function requisitos = obterRequisitos()
 %obtem os requisitos 
 	requisitos.controlador.wb = 0.4; % rad/s
-    requisitos.controlador.PM = 60 % em graus
-    requisitos.controlador.GM = 11 % em dB
+    requisitos.controlador.PM = 60; % em graus
+    requisitos.controlador.GM = 11; % em dB
 
 end
